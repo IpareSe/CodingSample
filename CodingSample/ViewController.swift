25b69a3677b091126e19425627c5e2106d864162
maulik patel <patelmaulik7909@gmail.com>
2019-02-04 14:35:47 +0530

    Initial Commit

---
+++ CodingSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CodingSample
+//
+//  Created by Kaira's Mac Mini on 04/02/19.
+//  Copyright © 2019 Kaira's Mac Mini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
